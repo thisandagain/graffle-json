1c33a4b823a8a8dc4cdb8cf073a80032f1bc68a7
Andrew Sliwinski <thisandagain@gmail.com>
2012-02-01 08:32:09 -0800

    Added more complex example and updated unit tests

--- test/test.js
+++ test/test.js
@@ -22,20 +22,48 @@ suite.addBatch({
             assert.isNull(err);  
         },
 
-        'is an object': function (err, obj) {
-            assert.isObject(obj);
+        'is an array': function (err, obj) {
+            assert.isArray(obj);
         },
 
         'equals expected result - Its': function (err, obj) {
-            assert.equal(obj.parse[0].value, "It's");
+            assert.equal(obj[0].value, "It's");
         },
 
         'equals expected result - Usually': function (err, obj) {
-            assert.equal(obj.parse[1].value, "Usually");
+            assert.equal(obj[1].value, "Usually");
         },
 
         'equals expected result - Child': function (err, obj) {
-            assert.equal(obj.parse[0].children[0].value, "To");
+            assert.equal(obj[0].children[0].value, "To");
+        },
+
+    },
+
+    'Parse (Complex)': {
+
+        topic: function() {
+            graffle.convert('./example/complex.oo3', this.callback);
+        },
+
+        'no error': function (err, obj) {
+            assert.isNull(err);  
+        },
+
+        'is an array': function (err, obj) {
+            assert.isArray(obj);
+        },
+
+        'equals expected result': function (err, obj) {
+            assert.equal(obj[0].value, "Jack");
+        },
+
+        'equals expected result': function (err, obj) {
+            assert.equal(obj[1].value, "And");
+        },
+
+        'equals expected result': function (err, obj) {
+            assert.equal(obj[0].children[0].value, "To");
         },
 
     },
